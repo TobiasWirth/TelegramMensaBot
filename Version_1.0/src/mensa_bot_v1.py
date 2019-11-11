01c46def14b30f73989fc1140f4b5668a858aebc
Tobias Wirth <tobias-jochen.wirth@stud.uni-bamberg.de>
2019-11-11 11:14:26 +0100

    Heroku

--- Version_1.0/src/mensa_bot_v1.py
+++ Version_1.0/src/mensa_bot_v1.py
@@ -19,11 +19,11 @@ import datetime
 from os import environ
 from flask import Flask
 
-#app = Flask(__name__)
-#app.run(host= '0.0.0.0', port=environ.get('PORT'))
-
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+app.run(host= '0.0.0.0', port=environ.get('PORT'))
+
+#app = Flask(__name__)
+#app.run(environ.get('PORT'))
 
 
 
